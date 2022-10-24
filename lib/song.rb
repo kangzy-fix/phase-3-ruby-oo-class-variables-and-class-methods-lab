c7389b4f70cc91c8391f3b344e3f8b6dd2719508
kangzy-fix <kangzy.fix@student.moringaschool.com>
2022-10-24 10:59:06 +0300

    added solution

---
+++ lib/song.rb
@@ -0,0 +1,83 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+  def self.artists
+    attr_reader :artist
+    @@artists.uniq
+  end
+  def self.genres
+    attr_reader :genre
+    @@genres.uniq
+  end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+end
+
+# Path: lib/artist.rb
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end
